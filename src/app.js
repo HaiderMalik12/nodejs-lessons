2ccd204975e0da3ee2a40c3c95a90112780b8743
Haider Malik <haidermalik504@gmail.com>
2018-05-05 09:45:49 +0500

    refactor code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-import { userRouter } from './api/user'
+import { userRouter } from './api/user';
+
 const app = express();
 const PORT = 3000;
+
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/eh_db');
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use('/users', userRouter);
+
 app.listen(PORT, () => {
     console.log(`App is running at http://localhost:${PORT}`)
 })
