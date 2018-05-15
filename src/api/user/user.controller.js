e0999199ea4d0b57bf9dd028d1897f5e1374d9f1
Haider Malik <haidermalik504@gmail.com>
2018-05-15 08:06:32 +0500

    Signup User

--- src/api/user/user.controller.js
+++ src/api/user/user.controller.js
@@ -1,7 +1,25 @@
+import bcrypt from 'bcryptjs';
 import User from './user.model';
+import userService from './user.service';
 
 export default {
   async signup(req, res) {
-    return res.json({ msg: 'TODO: Implement Signup' });
+    try {
+      const { value, error } = userService.validateSchema(req.body);
+      if (error) {
+        return res.status(400).send(error);
+      }
+      const user = new User();
+      user.local.email = value.email;
+
+      const salt = bcrypt.genSaltSync(10);
+      const hash = await bcrypt.hash(value.password, salt);
+      user.local.password = hash;
+      await user.save();
+      return res.json({ success: true, message: 'Signup Successful' });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
   }
 };
