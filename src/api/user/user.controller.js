d02d639286c7f232329251e0bac21beb2b51bca7
Haider Malik <haidermalik504@gmail.com>
2018-05-15 08:24:32 +0500

    login-user

--- src/api/user/user.controller.js
+++ src/api/user/user.controller.js
@@ -1,6 +1,8 @@
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import User from './user.model';
 import userService from './user.service';
+import { devConfig } from '../../config/development';
 
 export default {
   async signup(req, res) {
@@ -24,7 +26,22 @@ export default {
   },
   async login(req, res) {
     try {
-      return res.json({ msg: 'TODO: Login' });
+      const { error, value } = userService.validateSchema(req.body);
+      if (error) {
+        return res.status(400).send(error);
+      }
+      const user = await User.findOne({ 'local.email': value.email });
+      if (!user) {
+        return res.status(401).json({ err: 'Unauthorized' });
+      }
+      const matched = bcrypt.compareSync(value.password, user.local.password);
+      if (!matched) {
+        return res.status(400).json({ err: 'Bad credentials' });
+      }
+      const token = jwt.sign({ id: user._id }, devConfig.secret, {
+        expiresIn: '1d'
+      });
+      return res.json({ success: true, token });
     } catch (err) {
       console.error(err);
       return res.status(500).send(err);
