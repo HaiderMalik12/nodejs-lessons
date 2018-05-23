87679d0e1ff1d9a1e1ef40b4ea9d85c80f2049c1
Haider Malik <haidermalik504@gmail.com>
2018-05-23 11:34:51 +0500

    google auth setup

--- src/config/development.js
+++ src/config/development.js
@@ -9,5 +9,10 @@ export const devConfig = {
     clientId:  "bfa97f2edbdcb518d386",
     clientSecret: '97af87357a4ef0288e488c875bb3e2a5ad553a74',
     callbackURL: 'http://localhost:3000/users/auth/github/callback'
+  },
+  google:{
+    clientId: '376563107459-3egjvcn1s7sle9m7020neo53fa1u7ntb.apps.googleusercontent.com',
+    clientSecret: 'cR4LB5sI1tboDDrsbKJ1D9z0',
+    callbackURL: 'http://localhost:3000/users/google/callback'
   }
 };
